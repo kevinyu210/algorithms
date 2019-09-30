9b15bfda37975313ac4b8a5d369092838a301d03
Tony Yu <toekneeyou@Tonys-MacBook-Pro.local>
2019-09-29 17:24:38 -0700

    refactored code and added assumptions for each challenge as comments

--- java/ImpactJavaChallenge.java
+++ java/ImpactJavaChallenge.java
@@ -1,39 +1,29 @@
 import java.util.*;
 
 public class ImpactJavaChallenge {
-
     public static void main(String[] args) {
-        int x[] = {1, 1, 3, 1, 2, 3, 3, 3, 1};
-        System.out.println("toGoodString Tests:");
-        System.out.println(toGoodString("HellOOOOOO ThErEE"));
+        int x[] = {1, 5, 6, 1, 3, 1, 2, 3, 3, 3, 1, 6};
 
-        System.out.println("\nlistGenerator Tests:");
+        System.out.println("listGenerator Tests:");
         System.out.println(listGenerator(x));
-        System.out.println(listGenerator(new int[]{1,2,3}));
+        System.out.println(listGenerator(new int[]{1, 2, 3}));
+
+        System.out.println("\ntoGoodString Tests:");
+        System.out.println(toGoodString("hi there!!!!\\!"));
 
         System.out.println("\nduplicates Tests:");
         System.out.println(duplicates(x));
 
         System.out.println("\ncompleteValidator Tests:");
         System.out.println(completeValidator("Hi There"));
-        System.out.println(completeValidator("abcdefghijklmnopqrsTUVWXYZ"));
+        System.out.println(completeValidator("abcdefghijklmnopqrsTUVWXZzzzzzY"));
     }
 
-    private static String toGoodString(String x) {
-        String goodString = "";
-
-        for (int index = 0; index < x.length(); index++) {
-            goodString += x.charAt(index);
-            while (index < x.length() - 1) {
-                if (x.charAt(index) == x.charAt(index + 1)) {
-                    index++;
-                } else break;
-            }
-        }
-
-        return goodString;
-    }
-
+    // Challenge #1
+    // assume input is an array of whole numbers
+    // assume output is a string
+    // assume input array should be sorted so that numbers can be grouped in ranges
+    // keeping duplicates if present
     private static String listGenerator(int[] x) {
         String listOfNumbers = "";
 
@@ -59,20 +49,19 @@ public class ImpactJavaChallenge {
                 }
 
                 // all values between first and last
-                if (index > 0 && index < x.length - 1) {
-                    // if previous is not sequential
-                    if (x[index - 1] != x[index] - 1) {
-                        // if next is sequential
-                        if (x[index + 1] == x[index] + 1) listOfNumbers += x[index];
-                        // if next is not sequential
-                        if (x[index + 1] != x[index] + 1) listOfNumbers += (x[index] + ", ");
-                    }
+                if (index > 0 && index < (x.length - 1)) {
                     //if previous is sequential
                     if (x[index - 1] == x[index] - 1) {
-                        // if next is sequential
-                        if (x[index + 1] == x[index] + 1) index++;
                         // if next is not sequential
                         if (x[index + 1] != x[index] + 1) listOfNumbers += ("-" + x[index] + ", ");
+                        // if next is sequential, next iteration
+                    }
+                    // if previous is not sequential
+                    else {
+                        // if next is sequential
+                        if (x[index + 1] == x[index] + 1) listOfNumbers += x[index];
+                            // if next is not sequential
+                        else listOfNumbers += (x[index] + ", ");
                     }
                 }
             }
@@ -81,9 +70,32 @@ public class ImpactJavaChallenge {
         return listOfNumbers;
     }
 
+    // Challenge # 2
+    // assuming all lowercase
+    // not deleting duplicate punctuation marks
+    private static String toGoodString(String S) {
+        String punctuationMarks = ",.?!-;:()\"'";
+        String goodString = "";
+
+        for (int index = 0; index < S.length(); index++) {
+            goodString += S.charAt(index);
+            if (punctuationMarks.indexOf(S.charAt(index)) < 0) {
+                while (index < S.length() - 1) {
+                    if (S.charAt(index) == S.charAt(index + 1)) {
+                        index++;
+                    } else break;
+                }
+            }
+        }
+
+        return goodString;
+    }
+
+    // Challenge #3
+    // assume output returned in ascending order
     private static ArrayList<Integer> duplicates(int[] x) {
         Set<Integer> numbers = new HashSet<Integer>();
-        ArrayList<Integer> duplicates = new ArrayList<>(5);
+        ArrayList<Integer> duplicates = new ArrayList<>();
 
         for (int index = 0; index < x.length; index++) {
             if (!numbers.contains(x[index])) {
@@ -97,22 +109,18 @@ public class ImpactJavaChallenge {
         return duplicates;
     }
 
+    // Challenge #4
+    // instructions show a-z but will validate assuming letter case independent
     private static Boolean completeValidator(String x) {
         String lowerCaseString = x.toLowerCase();
-        Boolean isComplete = false;
         Set<Character> letters = new HashSet<Character>();
 
-        if (x.length() < 26) isComplete = false;
-        else {
+        if (x.length() >= 26) {
             for (int index = 0; index < lowerCaseString.length(); index++) {
-                if (!letters.contains(lowerCaseString.charAt(index))) {
-                    letters.add(lowerCaseString.charAt(index));
-                }
+                letters.add(lowerCaseString.charAt(index));
             }
         }
 
-        if (letters.size() == 26) isComplete = true;
-
-        return isComplete;
+        return letters.size() == 26;
     }
 }
